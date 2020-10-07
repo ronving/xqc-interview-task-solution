07f9c743d1c4dc50d16592fe25ee382f160140f6
Mykhailo <mykhailo_olkhovyi@epam.com>
2020-10-07 14:55:23 +0300

    initial commit

---
+++ src/main/java/com/ronving/king/demo/XqcTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ronving.king.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XqcTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XqcTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronving/king/demo/XqcTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronving.king.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XqcTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
